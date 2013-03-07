c6f72a3a55ac11a3e52c4e154e5dd4ff912862f9
Christopher Wellons <mosquitopsu@gmail.com>
2013-03-07 16:38:48 -0500

    Initial working generators.

---
+++ rng.js
@@ -0,0 +1,165 @@
+/**
+ * Seedable random number generator functions.
+ * @version 1.0
+ * @license Public Domain
+ *
+ * @example
+ * var rng = new RNG('Example');
+ * rng.uniform();      //  0.23486116157656023
+ * rng.normal();       // -0.19479177607547019
+ * rng.exponential();  //  0.5060762637166598
+ *
+ *   The generator here is about 10x slower than Math.random in
+ * V8. What you get in exchange for that is a seedable generator and
+ * additional random distributions (see example). You can still get
+ * speed *and* these additional distributions by using Math.random as
+ * the core generator.
+ *
+ * @example
+ * var rng = new RNG(Math.random);
+ *
+ *   When no seed is provided, one is created randomly from available
+ * entropy sources. Seeds that are not strings are run through
+ * JSON.stringify() to be used for seeding.
+ *
+ *   Replacing Math.random's generator with a seeded generator,
+ *
+ * @example
+ * Math.random = RNG.prototype.uniform.bind(new RNG('my seed'));
+ */
+
+/**
+ * Get the underlying bytes of this string.
+ * @returns {Array} An array of bytes
+ */
+String.prototype.getBytes = function() {
+    var output = [];
+    for (var i = 0; i < this.length; i++) {
+        var c = this.charCodeAt(i);
+        var bytes = [];
+        do {
+            bytes.push(c & 0xFF);
+            c = c >> 8;
+        } while (c > 0);
+        output = output.concat(bytes.reverse());
+    }
+    return output;
+};
+
+/**
+ * @param {String} seed A string to seed the generator.
+ * @constructor
+ */
+function RC4(seed) {
+    this.s = new Array(256);
+    this.i = 0;
+    this.j = 0;
+    for (var i = 0; i < 256; i++) {
+        this.s[i] = i;
+    }
+    if (seed) {
+        this.mix(seed);
+    }
+}
+
+RC4.prototype._swap = function(i, j) {
+    var tmp = this.s[i];
+    this.s[i] = this.s[j];
+    this.s[j] = tmp;
+};
+
+/**
+ * Mix additional entropy into this generator.
+ * @param {String} seed
+ */
+RC4.prototype.mix = function(seed) {
+    var input = seed.getBytes();
+    var j = 0;
+    for (var i = 0; i < this.s.length; i++) {
+        j += this.s[i] + input[i % input.length];
+        j %= 256;
+        this._swap(i, j);
+    }
+};
+
+/**
+ * @returns {number} The next byte of output from the generator.
+ */
+RC4.prototype.next = function() {
+    this.i = (this.i + 1) % 256;
+    this.j = (this.j + this.s[this.i]) % 256;
+    this._swap(this.i, this.j);
+    return this.s[(this.s[this.i] + this.s[this.j]) % 256];
+};
+
+/**
+ * Create a new random number generator with optional seed. If the
+ * provided seed is a function (i.e. Math.random) it will be used as
+ * the uniform number generator.
+ * @param seed An arbitrary object used to seed the generator.
+ * @constructor
+ */
+function RNG(seed) {
+    if (seed == null) {
+        seed = (Math.random() + Date.now()).toString();
+    } else if (typeof seed === "function") {
+        // Use it as a uniform number generator
+        this.uniform = seed;
+        this.nextByte = function() {
+            return ~~(this.uniform * 256);
+        };
+        seed = null;
+    } else if (Object.prototype.toString.call(seed) !== "[object String]") {
+        seed = JSON.stringify(seed);
+    }
+    this._normal = null;
+    if (seed) {
+        this._state = new RC4(seed);
+    } else {
+        this._state = null;
+    }
+}
+
+/**
+ * @returns {number} Uniform random number between 0 and 255.
+ */
+RNG.prototype.nextByte = function() {
+    return this._state.next();
+};
+
+/**
+ * @returns {number} Uniform random number between 0 and 1.
+ */
+RNG.prototype.uniform = function() {
+    var BYTES = 7; // 56 bits to make a 53-bit double
+    var output = 0;
+    for (var i = 0; i < BYTES; i++) {
+        output *= 256;
+        output += this.nextByte();
+    }
+    return output / (Math.pow(2, BYTES * 8) - 1);
+};
+
+/**
+ * Generates numbers using this.uniform() with the Box-Muller transform.
+ * @returns {number} Normally-distributed random number of mean 0, variance 1.
+ */
+RNG.prototype.normal = function() {
+    if (this._normal) {
+        var n = this._normal;
+        this._normal = null;
+        return n;
+    } else {
+        var x = this.uniform(), y = this.uniform();
+        this._normal = Math.sqrt(-2 * Math.log(x)) * Math.sin(2 * Math.PI * y);
+        return Math.sqrt(-2 * Math.log(x)) * Math.cos(2 * Math.PI * y);
+    }
+};
+
+/**
+ * Generates numbers using this.uniform().
+ * @returns {number} Number from the exponential distribution, lambda = 1.
+ */
+RNG.prototype.exponential = function() {
+    return -Math.log(this.uniform());
+};
